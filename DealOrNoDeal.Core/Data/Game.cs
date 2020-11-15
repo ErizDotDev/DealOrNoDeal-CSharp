869ea4300305203461493de4f097f83d49135806
Denzel dela Cruz <denzeldevwork01@gmail.com>
2020-11-15 23:20:36 +0800

    Placed initial data and process classes

---
+++ DealOrNoDeal.Core/Data/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealOrNoDeal.Core
+{
+   public class Game
+   {
+      public List<Briefcase> RemainingBriefcases { get; set; }
+      public List<Briefcase> RemovedBriefcases { get; set; }
+      public List<double> BankerOffers { get; set; }
+   }
+}
