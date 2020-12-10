2a143f7246264d9a9728f83fe79c063f5d7e0206
Denzel dela Cruz <denzeldevwork01@gmail.com>
2020-12-10 20:27:34 +0800

    Introduce extension methods

---
+++ DealOrNoDeal.Core/Extensions/PlayerExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace DealOrNoDeal.Core.Data
+{
+   public static class PlayerExtensions
+   {
+      public static bool IsValid(this Player player)
+      {
+         return player != null            
+            && player?.SelectedBriefcase != null
+            && !string.IsNullOrEmpty(player?.Name);
+      }
+   }
+}
